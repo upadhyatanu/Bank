26e50cc0e988d5d30a2c253de1afa978f957293f
tanushree <upadhyatanu@gmail.com>
2025-09-28 11:50:53 +0530

    added new constant in cards constants

--- Cards/src/main/java/com/eazybytes/cards/constants/CardsConstants.java
+++ Cards/src/main/java/com/eazybytes/cards/constants/CardsConstants.java
@@ -7,6 +7,7 @@ public final class CardsConstants {
     }
 
     public static final String  CREDIT_CARD = "Credit Card";
+    public static final String  DEBIT_CARD = "Debit Card";
     public static final int  NEW_CARD_LIMIT = 1_00_000;
     public static final String  STATUS_201 = "201";
     public static final String  MESSAGE_201 = "Card created successfully";
